c584ae9c81c6e1f297072219e96e87a555172e1f
Selbi182 <shadow93x@gmail.com>
2026-09-15 23:16:03 +0200

    Fixed SonLVL INI Files
    This mainly required fixing the file paths and compression types, but also needed to specify Sonic 3K's object and ring format. For this I had to add a dummy ring sprite in the Commons folder

--- _maps/Spiked Pole Helix.asm
+++ _maps/Spiked Pole Helix.asm
@@ -12,90 +12,90 @@ Map_Hel_internal:	mappingsTable
 	mappingsTableEntry.w	.opti7
 
 .opti0:	spriteHeader
-	spritePiece	-4-$40, 0, 1, 2, $E, 0, 0, 0, 0		; straight down
-	spritePiece	-3-$30, 4, 1, 1, $10, 0, 0, 0, 0	; 45 degree
-	;spritePiece	-0-$20, 0, 0, 0, $00, 0, 0, 0, 0	; blank
-	spritePiece	-3-$10, -$C, 1, 1, $11, 0, 0, 0, 0	; 45 degree
-	spritePiece	-4+$00, -$10, 1, 2, 0, 0, 0, 0, 0	; points straight up (harmful)
-	spritePiece	-8+$10, -$B, 2, 2, 2, 0, 0, 0, 0	; 45 degree
-	spritePiece	-8+$20, -8, 2, 2, 6, 0, 0, 0, 0		; 90 degree
-	spritePiece	-8+$30, -5, 2, 2, $A, 0, 0, 0, 0	; 45 degree
+	spritePiece	-68, 0, 1, 2, $E, 0, 0, 0, 0		; straight down
+	spritePiece	-51, 4, 1, 1, $10, 0, 0, 0, 0		; 45 degree
+	;spritePiece	-32, 0, 0, 0, $00, 0, 0, 0, 0		; blank
+	spritePiece	-19, -$C, 1, 1, $11, 0, 0, 0, 0		; 45 degree
+	spritePiece	-4, -$10, 1, 2, 0, 0, 0, 0, 0		; points straight up (harmful)
+	spritePiece	8, -$B, 2, 2, 2, 0, 0, 0, 0		; 45 degree
+	spritePiece	24, -8, 2, 2, 6, 0, 0, 0, 0		; 90 degree
+	spritePiece	40, -5, 2, 2, $A, 0, 0, 0, 0		; 45 degree
 .opti0_End
 
 .opti1:	spriteHeader
-	spritePiece	-3-$30, 4, 1, 1, $10, 0, 0, 0, 0	; 45 degree
-	;spritePiece	-0-$20, 0, 0, 0, $00, 0, 0, 0, 0	; blank
-	spritePiece	-3-$10, -$C, 1, 1, $11, 0, 0, 0, 0	; 45 degree
-	spritePiece	-4+$00, -$10, 1, 2, 0, 0, 0, 0, 0	; points straight up (harmful)
-	spritePiece	-8+$10, -$B, 2, 2, 2, 0, 0, 0, 0	; 45 degree
-	spritePiece	-8+$20, -8, 2, 2, 6, 0, 0, 0, 0		; 90 degree
-	spritePiece	-8+$30, -5, 2, 2, $A, 0, 0, 0, 0	; 45 degree
-	spritePiece	-4+$40, 0, 1, 2, $E, 0, 0, 0, 0		; straight down
+	spritePiece	-51, 4, 1, 1, $10, 0, 0, 0, 0		; 45 degree
+	;spritePiece	-32, 0, 0, 0, $00, 0, 0, 0, 0		; blank
+	spritePiece	-19, -$C, 1, 1, $11, 0, 0, 0, 0		; 45 degree
+	spritePiece	-4, -$10, 1, 2, 0, 0, 0, 0, 0		; points straight up (harmful)
+	spritePiece	8, -$B, 2, 2, 2, 0, 0, 0, 0		; 45 degree
+	spritePiece	24, -8, 2, 2, 6, 0, 0, 0, 0		; 90 degree
+	spritePiece	40, -5, 2, 2, $A, 0, 0, 0, 0		; 45 degree
+	spritePiece	60, 0, 1, 2, $E, 0, 0, 0, 0		; straight down
 .opti1_End
 
 .opti2:	spriteHeader
-	;spritePiece	-0-$20, 0, 0, 0, $00, 0, 0, 0, 0	; blank
-	spritePiece	-3-$10, -$C, 1, 1, $11, 0, 0, 0, 0	; 45 degree
-	spritePiece	-4+$00, -$10, 1, 2, 0, 0, 0, 0, 0	; points straight up (harmful)
-	spritePiece	-8+$10, -$B, 2, 2, 2, 0, 0, 0, 0	; 45 degree
-	spritePiece	-8+$20, -8, 2, 2, 6, 0, 0, 0, 0		; 90 degree
-	spritePiece	-8+$30, -5, 2, 2, $A, 0, 0, 0, 0	; 45 degree
-	spritePiece	-4+$40, 0, 1, 2, $E, 0, 0, 0, 0		; straight down
-	spritePiece	-3+$50, 4, 1, 1, $10, 0, 0, 0, 0	; 45 degree
+	;spritePiece	-32, 0, 0, 0, $00, 0, 0, 0, 0		; blank
+	spritePiece	-19, -$C, 1, 1, $11, 0, 0, 0, 0		; 45 degree
+	spritePiece	-4, -$10, 1, 2, 0, 0, 0, 0, 0		; points straight up (harmful)
+	spritePiece	8, -$B, 2, 2, 2, 0, 0, 0, 0		; 45 degree
+	spritePiece	24, -8, 2, 2, 6, 0, 0, 0, 0		; 90 degree
+	spritePiece	40, -5, 2, 2, $A, 0, 0, 0, 0		; 45 degree
+	spritePiece	60, 0, 1, 2, $E, 0, 0, 0, 0		; straight down
+	spritePiece	77, 4, 1, 1, $10, 0, 0, 0, 0		; 45 degree
 .opti2_End
 
 .opti3:	spriteHeader
-	spritePiece	-3-$10, -$C, 1, 1, $11, 0, 0, 0, 0	; 45 degree
-	spritePiece	-4+$00, -$10, 1, 2, 0, 0, 0, 0, 0	; points straight up (harmful)
-	spritePiece	-8+$10, -$B, 2, 2, 2, 0, 0, 0, 0	; 45 degree
-	spritePiece	-8+$20, -8, 2, 2, 6, 0, 0, 0, 0		; 90 degree
-	spritePiece	-8+$30, -5, 2, 2, $A, 0, 0, 0, 0	; 45 degree
-	spritePiece	-4+$40, 0, 1, 2, $E, 0, 0, 0, 0		; straight down
-	spritePiece	-3+$50, 4, 1, 1, $10, 0, 0, 0, 0	; 45 degree
-	;spritePiece	-0+$60, 0, 0, 0, $00, 0, 0, 0, 0	; blank
+	spritePiece	-19, -$C, 1, 1, $11, 0, 0, 0, 0		; 45 degree
+	spritePiece	-4, -$10, 1, 2, 0, 0, 0, 0, 0		; points straight up (harmful)
+	spritePiece	8, -$B, 2, 2, 2, 0, 0, 0, 0		; 45 degree
+	spritePiece	24, -8, 2, 2, 6, 0, 0, 0, 0		; 90 degree
+	spritePiece	40, -5, 2, 2, $A, 0, 0, 0, 0		; 45 degree
+	spritePiece	60, 0, 1, 2, $E, 0, 0, 0, 0		; straight down
+	spritePiece	77, 4, 1, 1, $10, 0, 0, 0, 0		; 45 degree
+	;spritePiece	96, 0, 0, 0, $00, 0, 0, 0, 0		; blank
 .opti3_End
 
 .opti4:	spriteHeader
-	spritePiece	-4+$00, -$10, 1, 2, 0, 0, 0, 0, 0	; points straight up (harmful)
-	spritePiece	-8+$10, -$B, 2, 2, 2, 0, 0, 0, 0	; 45 degree
-	spritePiece	-8+$20, -8, 2, 2, 6, 0, 0, 0, 0		; 90 degree
-	spritePiece	-8+$30, -5, 2, 2, $A, 0, 0, 0, 0	; 45 degree
-	spritePiece	-4+$40, 0, 1, 2, $E, 0, 0, 0, 0		; straight down
-	spritePiece	-3+$50, 4, 1, 1, $10, 0, 0, 0, 0	; 45 degree
-	;spritePiece	-0+$60, 0, 0, 0, $00, 0, 0, 0, 0	; blank
-	spritePiece	-3+$70, -$C, 1, 1, $11, 0, 0, 0, 0	; 45 degree
+	spritePiece	-4, -$10, 1, 2, 0, 0, 0, 0, 0		; points straight up (harmful)
+	spritePiece	8, -$B, 2, 2, 2, 0, 0, 0, 0		; 45 degree
+	spritePiece	24, -8, 2, 2, 6, 0, 0, 0, 0		; 90 degree
+	spritePiece	40, -5, 2, 2, $A, 0, 0, 0, 0		; 45 degree
+	spritePiece	60, 0, 1, 2, $E, 0, 0, 0, 0		; straight down
+	spritePiece	77, 4, 1, 1, $10, 0, 0, 0, 0		; 45 degree
+	;spritePiece	96, 0, 0, 0, $00, 0, 0, 0, 0		; blank
+	spritePiece	109, -$C, 1, 1, $11, 0, 0, 0, 0		; 45 degree
 .opti4_End
 
 .opti5:	spriteHeader
-	spritePiece	-8-$70, -$B, 2, 2, 2, 0, 0, 0, 0	; 45 degree
-	spritePiece	-8-$60, -8, 2, 2, 6, 0, 0, 0, 0		; 90 degree
-	spritePiece	-8-$50, -5, 2, 2, $A, 0, 0, 0, 0	; 45 degree
-	spritePiece	-4-$40, 0, 1, 2, $E, 0, 0, 0, 0		; straight down
-	spritePiece	-3-$30, 4, 1, 1, $10, 0, 0, 0, 0	; 45 degree
-	;spritePiece	-0-$20, 0, 0, 0, $00, 0, 0, 0, 0	; blank
-	spritePiece	-3-$10, -$C, 1, 1, $11, 0, 0, 0, 0	; 45 degree
-	spritePiece	-4+$00, -$10, 1, 2, 0, 0, 0, 0, 0	; points straight up (harmful)
+	spritePiece	-120, -$B, 2, 2, 2, 0, 0, 0, 0		; 45 degree
+	spritePiece	-104, -8, 2, 2, 6, 0, 0, 0, 0		; 90 degree
+	spritePiece	-88, -5, 2, 2, $A, 0, 0, 0, 0		; 45 degree
+	spritePiece	-68, 0, 1, 2, $E, 0, 0, 0, 0		; straight down
+	spritePiece	-51, 4, 1, 1, $10, 0, 0, 0, 0		; 45 degree
+	;spritePiece	-32, 0, 0, 0, $00, 0, 0, 0, 0		; blank
+	spritePiece	-19, -$C, 1, 1, $11, 0, 0, 0, 0		; 45 degree
+	spritePiece	-4, -$10, 1, 2, 0, 0, 0, 0, 0		; points straight up (harmful)
 .opti5_End
 
 .opti6:	spriteHeader
-	spritePiece	-8-$60, -8, 2, 2, 6, 0, 0, 0, 0		; 90 degree
-	spritePiece	-8-$50, -5, 2, 2, $A, 0, 0, 0, 0	; 45 degree
-	spritePiece	-4-$40, 0, 1, 2, $E, 0, 0, 0, 0		; straight down
-	spritePiece	-3-$30, 4, 1, 1, $10, 0, 0, 0, 0	; 45 degree
-	;spritePiece	-0-$20, 0, 0, 0, $00, 0, 0, 0, 0	; blank
-	spritePiece	-3-$10, -$C, 1, 1, $11, 0, 0, 0, 0	; 45 degree
-	spritePiece	-4+$00, -$10, 1, 2, 0, 0, 0, 0, 0	; points straight up (harmful)
-	spritePiece	-8+$10, -$B, 2, 2, 2, 0, 0, 0, 0	; 45 degree
+	spritePiece	-104, -8, 2, 2, 6, 0, 0, 0, 0		; 90 degree
+	spritePiece	-88, -5, 2, 2, $A, 0, 0, 0, 0		; 45 degree
+	spritePiece	-68, 0, 1, 2, $E, 0, 0, 0, 0		; straight down
+	spritePiece	-51, 4, 1, 1, $10, 0, 0, 0, 0		; 45 degree
+	;spritePiece	-32, 0, 0, 0, $00, 0, 0, 0, 0		; blank
+	spritePiece	-19, -$C, 1, 1, $11, 0, 0, 0, 0		; 45 degree
+	spritePiece	-4, -$10, 1, 2, 0, 0, 0, 0, 0		; points straight up (harmful)
+	spritePiece	8, -$B, 2, 2, 2, 0, 0, 0, 0		; 45 degree
 .opti6_End
 
 .opti7:	spriteHeader
-	spritePiece	-8-$50, -5, 2, 2, $A, 0, 0, 0, 0	; 45 degree
-	spritePiece	-4-$40, 0, 1, 2, $E, 0, 0, 0, 0		; straight down
-	spritePiece	-3-$30, 4, 1, 1, $10, 0, 0, 0, 0	; 45 degree
-	;spritePiece	-0-$20, 0, 0, 0, $00, 0, 0, 0, 0	; blank
-	spritePiece	-3-$10, -$C, 1, 1, $11, 0, 0, 0, 0	; 45 degree
-	spritePiece	-4+$00, -$10, 1, 2, 0, 0, 0, 0, 0	; points straight up (harmful)
-	spritePiece	-8+$10, -$B, 2, 2, 2, 0, 0, 0, 0	; 45 degree
-	spritePiece	-8+$20, -8, 2, 2, 6, 0, 0, 0, 0		; 90 degree
+	spritePiece	-88, -5, 2, 2, $A, 0, 0, 0, 0		; 45 degree
+	spritePiece	-68, 0, 1, 2, $E, 0, 0, 0, 0		; straight down
+	spritePiece	-51, 4, 1, 1, $10, 0, 0, 0, 0		; 45 degree
+	;spritePiece	-32, 0, 0, 0, $00, 0, 0, 0, 0		; blank
+	spritePiece	-19, -$C, 1, 1, $11, 0, 0, 0, 0		; 45 degree
+	spritePiece	-4, -$10, 1, 2, 0, 0, 0, 0, 0		; points straight up (harmful)
+	spritePiece	8, -$B, 2, 2, 2, 0, 0, 0, 0		; 45 degree
+	spritePiece	24, -8, 2, 2, 6, 0, 0, 0, 0		; 90 degree
 .opti7_End
 	even
